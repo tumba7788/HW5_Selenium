b37a7570a05063cee6bad51434fd499b0f437fea
tumba7788 <Elena_Danilova7788@outlook.com>
2025-03-14 21:41:57 +0100

    v1

---
+++ README.md
@@ -0,0 +1,6 @@
+Домашнее задание для урока 5 - Поиск локаторов
+
+1. Откройте страницу https://bonigarcia.dev/selenium-webdriver-java/web-form.html  (https://bonigarcia.dev/selenium-webdriver-java/web-form.html)
+2. Написать локаторы для всех элементов
+3. Использовать лучшие локаторы
+4. Задание со звездочкой: Напишите по локатору каждого типа минимум 1 раз
--- src/main/java/Danilova/Main.java
+++ src/main/java/Danilova/Main.java
@@ -1,17 +1,42 @@
 package Danilova;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final String WEB_FORM_URL = "https://bonigarcia.dev/selenium-webdriver-java/web-form.html";
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       getPageLabelsTest();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+    //Этот метод нужен для того, чтобы автоматически получить названия всех лэйблов со страницы
+    static void getPageLabelsTest() {
+        WebDriver driver= new ChromeDriver();
+            driver.get(WEB_FORM_URL);
+            driver.manage().window().maximize();
+
+        List<WebElement> fields = driver.findElements(By.xpath("//label[contains(@class,'form-label w-100') or contains(@class,'form-check-label w-100')]"));
+
+        try (FileWriter writer = new FileWriter("titlesWebFormPage.txt", false)) {  // Appending to the file
+            for (WebElement webElement : fields) {
+                String field = webElement.getText().split("\n")[0].trim();//label возвращает текст из вложенных элементов, поэтому split("\n") — разбивает текст по строкам.[0] — берет первую строку.trim() — убирает пробелы.
+
+                // Output and write the title to a file
+                System.out.println("Page field: " + field);
+                writer.write(field + "\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Error during writing fields: " + e.getMessage());
         }
+        driver.quit();
     }
 }
